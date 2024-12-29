89d89e03e973064442c1326ed90cb8f8f857716c
Raygama <daffaraygama55@gmail.com>
2024-12-29 21:00:27 +0700

    first commit

---
+++ src/main/java/com/sister/sister15/Sister15Application.java
@@ -0,0 +1,20 @@
+package com.sister.sister15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Sister15Application {
+
+    @GetMapping("/messages")
+    public String Message() {
+        return "Tugas 15 Cloud Computing - Azure Web Application";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Sister15Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sister/sister15/Sister15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sister.sister15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sister15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
